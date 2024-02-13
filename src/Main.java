5b13af03d2887782fa80d4b20dc37eff17a0a387
Alex Gaina <alex.gaina@mail.ru>
2024-02-13 19:35:59 +0300

    2texts remove

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Иди на хуй");
-        System.out.println("Сам иди на хуй");
+        System.out.println("Кто? Я?");
     }
 }
